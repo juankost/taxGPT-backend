719bd275b803af9f4aa2239924d5a2bedaab4b10
juankost <juan.kostelec@gmail.com>
2024-02-24 17:14:15 +0100

    adsfasdfasdvsdfv, sdfkhv sdpkhcb öksdfg

--- app/api/database_interface.py
+++ app/api/database_interface.py
@@ -19,13 +19,12 @@ def get_topk_context_chunks(query, k=10, max_context_length=4096):
         os.environ["DATABASE_IP_ADDRESS"] = fetch_database_ip()
 
     # TODO There must be a better way to do this
-    logging.info(f"Sending request to {db_api_endpoint}")
 
     # db_api_endpoint = "http://" + os.getenv("DATABASE_IP_ADDRESS") + ":" + os.getenv("DATABASE_PORT") + "/get_context"
     # response = requests.post(db_api_endpoint, json={"query": query, "k": k, "max_context_length": max_context_length})
     db_api_endpoint = "http://" + os.getenv("DATABASE_IP_ADDRESS") + ":" + os.getenv("DATABASE_PORT") + "/"
+    logging.info(f"Sending request to {db_api_endpoint}")
     response = requests.get(db_api_endpoint)
-
     if response.status_code == 200:
         logging.info(f"Received response from {db_api_endpoint}")
         context = response.json().get("message")
