9be3121b6c90c561b0ab9683eb3defa27cc1af61
juankost <juan.kostelec@gmail.com>
2024-02-24 17:10:01 +0100

    testing hellow world request for database

--- app/api/database_interface.py
+++ app/api/database_interface.py
@@ -15,16 +15,20 @@ def fetch_database_ip():
 
 
 def get_topk_context_chunks(query, k=10, max_context_length=4096):
-    # if os.getenv("DATABASE_IP_ADDRESS") is None:
-    os.environ["DATABASE_IP_ADDRESS"] = fetch_database_ip()
+    if os.getenv("DATABASE_IP_ADDRESS") is None:
+        os.environ["DATABASE_IP_ADDRESS"] = fetch_database_ip()
 
     # TODO There must be a better way to do this
-    db_api_endpoint = "http://" + os.getenv("DATABASE_IP_ADDRESS") + ":" + os.getenv("DATABASE_PORT") + "/get_context"
     logging.info(f"Sending request to {db_api_endpoint}")
-    print("Sending request to", db_api_endpoint)
-    response = requests.post(db_api_endpoint, json={"query": query, "k": k, "max_context_length": max_context_length})
+
+    # db_api_endpoint = "http://" + os.getenv("DATABASE_IP_ADDRESS") + ":" + os.getenv("DATABASE_PORT") + "/get_context"
+    # response = requests.post(db_api_endpoint, json={"query": query, "k": k, "max_context_length": max_context_length})
+    db_api_endpoint = "http://" + os.getenv("DATABASE_IP_ADDRESS") + ":" + os.getenv("DATABASE_PORT") + "/"
+    response = requests.get(db_api_endpoint)
+
     if response.status_code == 200:
-        context = response.json().get("context")
+        logging.info(f"Received response from {db_api_endpoint}")
+        context = response.json().get("message")
         return context
     else:
         # Return error message
