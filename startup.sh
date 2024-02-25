884c7a695e5d7e83f1f9b50f0fdb9183f3c8358d
juankost <juan.kostelec@gmail.com>
2024-02-25 11:55:38 +0100

    export in caps instead of lowercoase

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch the PORT environment variable or default to 8000
-EXPORT PORT=${PORT:-8000}
+export PORT=${PORT:-8000}
 
 # Start Uvicorn with dynamic port assignment
 # exec uvicorn app.app:app --host 0.0.0.0 --port ${PORT}  # app.app is still ok becuase the workdir is /workspace/src
